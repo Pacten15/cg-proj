598b6af21b0ff2930f5b63ee65675af42bb927da
ist199107 <mario.pereira@tecnico.ulisboa.pt>
2023-05-15 22:45:06 +0100

    Implementação das 5 câmaras e do Resize

--- B/js/main-script.js
+++ B/js/main-script.js
@@ -2,7 +2,7 @@
 /* GLOBAL VARIABLES */
 //////////////////////
 
-var scene, cameras = [], renderer;
+var scene, cameras = [], renderer, current_cam;
 
 
 /////////////////////
@@ -11,80 +11,34 @@ var scene, cameras = [], renderer;
 function createScene() {
     'use strict';
     scene = new THREE.Scene();
+    scene.background = new THREE.Color("rgb(90%, 90%, 90%)");
     scene.add(new THREE.AxisHelper(10));
 }
 
 //////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
-function createPerspectiveCamera() {
+
+function createOrthographicCamera(x, y, z) {
     'use strict';
-    var camera = new THREE.PerspectiveCamera(70,
-                                         window.innerWidth / window.innerHeight,
-                                         1,
-                                         1000);
-    camera.position.x = 50;
-    camera.position.y = 50;
-    camera.position.z = 50;
-    camera.lookAt(scene.position);
+    var camera = new THREE.OrthographicCamera( - 60 * window.innerWidth / window.innerHeight, 60 * window.innerWidth / window.innerHeight, 60, - 60, 1, 1000 );
+    camera.position.x = x;
+    camera.position.y = y;
+    camera.position.z = z;
+    camera.lookAt( scene.position );
 
     cameras.push(camera);
 }
 
-function createPerspectiveCamera() {
+function createPerspectiveCamera(x, y, z) {
     'use strict';
-    var camera = new THREE.Camera(70,
-                                         window.innerWidth / window.innerHeight,
-                                         1,
-                                         1000);
-    camera.position.x = 50;
-    camera.position.y = 50;
-    camera.position.z = 50;
+    var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.x = x;
+    camera.position.y = y;
+    camera.position.z = z;
     camera.lookAt(scene.position);
 
     cameras.push(camera);
-
-}
-
-function createPerspectiveCamera() {
-    'use strict';
-    var camera = new THREE.PerspectiveCamera(70,
-                                         window.innerWidth / window.innerHeight,
-                                         1,
-                                         1000);
-    camera.position.x = 50;
-    camera.position.y = 50;
-    camera.position.z = 50;
-    camera.lookAt(scene.position);
-
-    cameras.push(camera);
-}
-
-function createPerspectiveCamera() {
-    'use strict';
-    var camera = new THREE.PerspectiveCamera(70,
-                                         window.innerWidth / window.innerHeight,
-                                         1,
-                                         1000);
-    camera.position.x = 50;
-    camera.position.y = 50;
-    camera.position.z = 50;
-    camera.lookAt(scene.position);
-
-    cameras.push(camera);
-}
-
-function createOrtographicCamera() {
-    var camera = new THREE.PerspectiveCamera(70,
-        window.innerWidth / window.innerHeight,
-        1,
-        1000);
-        camera.position.x = 50;
-        camera.position.y = 50;
-        camera.position.z = 50;
-        camera.lookAt(scene.position);
-
-        cameras.push(camera);
 }
 
 /////////////////////
@@ -99,9 +53,9 @@ function createCube(x, y, z) {
 
     var cube = new THREE.Object3D();
 
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true });
+    var material = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true });
 
-    var geometry = new THREE.BoxGeometry(5, 5, 5);
+    var geometry = new THREE.BoxGeometry(15, 15, 15);
     
     var mesh = new THREE.Mesh(geometry, material);
     
@@ -143,9 +97,10 @@ function update(){
 /////////////
 /* DISPLAY */
 /////////////
-function render() {
+function render(camera) {
     'use strict';
-    renderer.render(scene, cameras[0]);
+    current_cam = camera;
+    renderer.render(scene, camera);
 }
 
 ////////////////////////////////
@@ -158,11 +113,18 @@ function init() {
     document.body.appendChild(renderer.domElement);
 
     createScene();
-    createCamera();
+    createOrthographicCamera(0, 0, 70);     //frontal (vista para xy)
+    createOrthographicCamera(70, 0, 0);     //lateral (vista para yz)
+    createOrthographicCamera(0, 70, 0);     //topo (vista para xz)
+    createOrthographicCamera(50, 50, -50);  //perspetiva isométrica (projeção ortogonal)
+    createPerspectiveCamera(50, 50, 50);    //perspetiva isométrica (projeção perspetiva)
 
     createCube(0, 0, 0);
 
-    render();
+    render(cameras[0]);
+
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("resize", onResize);
 }
 
 /////////////////////
@@ -170,7 +132,7 @@ function init() {
 /////////////////////
 function animate() {
     'use strict';
-    render();
+    render(current_cam);
 }
 
 ////////////////////////////
@@ -178,6 +140,15 @@ function animate() {
 ////////////////////////////
 function onResize() { 
     'use strict';
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    if (window.innerHeight > 0 && window.innerWidth > 0) {
+        for (let i = 0; i < 5; i++) {
+            cameras[i].aspect = window.innerWidth / window.innerHeight;
+            cameras[i].updateProjectionMatrix();
+        }
+    }
+    render(current_cam);
 
 }
 
@@ -187,6 +158,15 @@ function onResize() {
 function onKeyDown(e) {
     'use strict';
 
+    switch (e.keyCode) {
+    case 49:
+    case 50:
+    case 51:
+    case 52:
+    case 53:
+        render(cameras[e.keyCode - 49]);
+        break;
+    }
 }
 
 ///////////////////////
