9e834791ed3003032d0ad04510ae6834f4e9acff
Luís Filipe Pedro Marques <luis.p.marques@tecnico.ulisboa.pt>
2023-05-31 16:58:42 +0100

    sky dome almost done missing textures and inclusion of the environment

--- C/js/main-script.js
+++ C/js/main-script.js
@@ -2,19 +2,41 @@
 /* GLOBAL VARIABLES */
 //////////////////////
 
+var scene, cameras = [], renderer, currentCam;
+
+var geometry, material, mesh;
+
+var skyDomeGeo, skyDome;
+
+var loader, texture;
+
+
+// colors
+const red = 0xFF0000, blue = 0x0000FF, yellow = 0xFFFF00, gray = 0x999999, darkGray = 0x555555, black = 0x000000;
 
 /////////////////////
 /* CREATE SCENE(S) */
 /////////////////////
 function createScene(){
     'use strict';
-
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color("rgb(90%, 90%, 90%)");
+    scene.add(new THREE.AxesHelper(10));
+    createSkydome();
+    
 }
 
 //////////////////////
 /* CREATE CAMERA(S) */
 //////////////////////
 
+function createPerspectiveCamera(x, y, z) {
+    'use strict';
+    var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.set(x, y, z);
+    camera.lookAt(scene.position);
+    cameras.push(camera);
+}
 
 /////////////////////
 /* CREATE LIGHT(S) */
@@ -23,6 +45,50 @@ function createScene(){
 ////////////////////////
 /* CREATE OBJECT3D(S) */
 ////////////////////////
+
+
+function createCube(obj, x, y, z, width, height, depth, color) {
+    'use strict';
+    material = new THREE.MeshBasicMaterial({ color: color, wireframe: false });
+    geometry = new THREE.BoxGeometry(width, height, depth);
+    mesh = new THREE.Mesh(geometry, material)
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+    return mesh;
+}
+
+function createCylinder(obj, x, y, z) {
+    'use strict';
+    material = new THREE.MeshBasicMaterial({ color: black, wireframe: false });
+    geometry = new THREE.CylinderGeometry(wheelRadius, wheelRadius, wheelHeight,32);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.rotation.x = Math.PI/2;
+    mesh.rotation.z = Math.PI/2;
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+    return mesh;
+}
+
+function createCone(obj, x, y, z, radius, height, color) {
+    'use strict';
+    material = new THREE.MeshBasicMaterial({ color: color, wireframe: false });
+    geometry = new THREE.ConeGeometry(radius, height);
+    mesh = new THREE.Mesh(geometry, material);
+    mesh.position.set(x, y, z);
+    obj.add(mesh);
+    return mesh;
+}
+
+function createSkydome() {
+    geometry = new THREE.SphereGeometry(1000, 80, 32);
+    const textureLoader = new THREE.TextureLoader();
+    //Estou a usar uma imagem random mas quando tiveremos as texturas feitas a do céu estrelado coloca-se aqui\\
+    const texture = textureLoader.load("js/2823368.jpg");
+    const material = new THREE.MeshBasicMaterial({ map: texture });
+    sky = new THREE.Mesh(geometry, material);
+    sky.position.set(0,0,0);
+    scene.add(sky);
+}
 
 //////////////////////
 /* CHECK COLLISIONS */
@@ -44,16 +110,19 @@ function handleCollisions(){
 /* UPDATE */
 ////////////
 function update(){
+
     'use strict';
 
 }
 
+
 /////////////
 /* DISPLAY */
 /////////////
-function render() {
+function render(camera) {
     'use strict';
-
+    currentCam = camera;
+    renderer.render(scene, camera);
 }
 
 ////////////////////////////////
@@ -61,6 +130,15 @@ function render() {
 ////////////////////////////////
 function init() {
     'use strict';
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+
+    createScene();
+    createPerspectiveCamera(1000, 1000, 1000);    //perspetiva isométrica (projeção perspetiva)
+    render(cameras[0]);
+
 
 }
 
@@ -69,7 +147,11 @@ function init() {
 /////////////////////
 function animate() {
     'use strict';
+    update();
+    
+    render(currentCam);
 
+    requestAnimationFrame(animate);
 }
 
 ////////////////////////////
@@ -77,7 +159,14 @@ function animate() {
 ////////////////////////////
 function onResize() { 
     'use strict';
-
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    if (window.innerHeight > 0 && window.innerWidth > 0) {
+        for (let i = 0; i < 5; i++) {
+            cameras[0].aspect = window.innerWidth / window.innerHeight;
+            cameras[0].updateProjectionMatrix();
+        }
+    }
+    render(currentCam);
 }
 
 ///////////////////////
